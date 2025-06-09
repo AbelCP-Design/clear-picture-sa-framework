bf90ff79de3e84f6d3c61b2b9fda039dd6d41ef2
apad22CPS <abel.padilla@clearpicturesa.com>
2025-06-09 16:44:17 -0500

    -

--- observablehq.config.js
+++ observablehq.config.js
@@ -44,5 +44,5 @@ pages: [
   // typographer: false, // smart quotes and other typographic improvements
   // preserveExtension: false, // drop .html from URLs
   // preserveIndex: false, // drop /index from URLs
-  // output: "/dist"
+  output: "/dist"
 };
